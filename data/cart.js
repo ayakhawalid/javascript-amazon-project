1dd33dadf97466506358a8cc1c7a4b84a53cafe4
aya <aia.khaw110@gmail.com>
2026-04-18 14:38:11 +0300

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,11 @@
-export const cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2}
+export let cart = [
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2},
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 3
+  }
 ];
 
 export function addToCart(productId, quantity)
@@ -16,4 +21,14 @@ export function addToCart(productId, quantity)
   })
   if(!found)
     cart.push({productId, quantity});
+}
+
+export function removeFromCart(productId)
+{
+  const newCart = [];
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId != productId)
+      newCart.push(cartItem);
+  })
+  cart = newCart;
 }
