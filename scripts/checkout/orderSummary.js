03a42349810c93ec1d5a7e5961be15e452dba04b
aya <aia.khaw110@gmail.com>
2026-04-20 15:59:28 +0300

    make payment summary interactive: delete and delivery option update

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import formatCurrency from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary()
@@ -71,6 +72,7 @@ export function renderOrderSummary()
       removeFromCart(productId);
       document.querySelector(`.js-cart-item-container-${productId}`).remove();
       updateCartQuantity();
+      renderPaymentSummary();
     })
   });
 
@@ -135,6 +137,7 @@ export function renderOrderSummary()
     const {productId, deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId, deliveryOptionId);
     renderOrderSummary();
+    renderPaymentSummary();
   })
   })
 
